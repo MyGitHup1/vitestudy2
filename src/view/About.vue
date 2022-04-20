cabd6e50d7ffe414a6c9f8a7251d3a6801d937de
1411089647@qq.com <leige123>
2022-04-20 22:26:05 +0800

    0001

--- src/view/About.vue
+++ src/view/About.vue
@@ -6,11 +6,12 @@
   <ph-activity-light></ph-activity-light>
   <ph-apple-logo-fill ></ph-apple-logo-fill>
   <mdi-ab-testing></mdi-ab-testing>
-  <audio autoplay  muted controls src="src/assets/于洋 - 望爱却步.mp3"></audio>
+  <audio autoplay  muted controls src="于洋 - 望爱却步.mp3"></audio>
 </template>
 <script>
 import {ref} from "vue";
 import RenderFun from "./RenderFun.vue";
+import  MP3 from "../../public/于洋 - 望爱却步.mp3"
 
 export default {
   name: "About",
